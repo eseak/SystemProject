58fafa0db4811f0c96f2ee282a801451ca12cd57
eseak <eseak@hotmail.comm>
2016-04-07 20:44:52 +0300

    Son commit: 7 Nisan 2016 20:45

--- FileBrowser1/formLayoutMain2.py
+++ FileBrowser1/formLayoutMain2.py
@@ -2,16 +2,16 @@
 __author__ = 'Ese'
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 
-from formLayout import Ui_MainWindow
+from PyQt5.QtWidgets import *
+
+
+from formLayout2 import Ui_MainWindow
 
 app = QApplication(sys.argv)
 window = QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(window)
-window.setWindowTitle("Layout...")
+window.setWindowTitle("Dosya Gezgini")
 window.show()
 sys.exit(app.exec_())
